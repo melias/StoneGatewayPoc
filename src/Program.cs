1f4128c84141011dd5c79ce808258277e97f4e7a
Maykon Elias <melias@direct.one>
2018-03-23 09:19:28 -0300

    add option to cancel transaction

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Configuration;
+using System.Linq;
 using System.Net;
 using GatewayApiClient;
 using GatewayApiClient.DataContracts;
 using GatewayApiClient.DataContracts.EnumTypes;
+using GatewayApiClient.EnumTypes;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -37,6 +39,7 @@ namespace StoneGatewayPoc
              */
             #endregion
 
+            var begin = 7;
             Console.SetCursorPosition(1, 1);
             Console.WriteLine("choice a option:");
             Console.SetCursorPosition(1, 2);
@@ -45,7 +48,9 @@ namespace StoneGatewayPoc
             Console.WriteLine("2 - to find a credit card");
             Console.SetCursorPosition(1, 4);
             Console.WriteLine("3 - to create a transaction fake");
-            Console.SetCursorPosition(1, 6);
+            Console.SetCursorPosition(1, 5);
+            Console.WriteLine("4 - to cancel a transaction");
+            Console.SetCursorPosition(1, begin);
             var readKey = Console.Read();
             var serializerSettings = new JsonSerializerSettings
             {
@@ -68,9 +73,9 @@ namespace StoneGatewayPoc
                     var createCreditCardResponse = client.CreditCard.CreateCreditCard(createInstantBuyDataRequest);
                     if (createCreditCardResponse.HttpStatusCode == HttpStatusCode.Created && createCreditCardResponse.Response.Success == true)
                     {
-                        Console.SetCursorPosition(1, 6);
+                        Console.SetCursorPosition(1, begin);
                         Console.Write(new string(' ', Console.WindowWidth));
-                        Console.SetCursorPosition(1, 6);
+                        Console.SetCursorPosition(1, begin);
                         Console.WriteLine("InstantBuy Key: {0}", createCreditCardResponse.Response.InstantBuyKey);
                     }
                     break;
@@ -82,9 +87,9 @@ namespace StoneGatewayPoc
                     var obj = JsonConvert.DeserializeObject(result);
                     var json = JsonConvert.SerializeObject(obj, serializerSettings);
                     //
-                    Console.SetCursorPosition(1, 6);
+                    Console.SetCursorPosition(1, begin);
                     Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(1, 6);
+                    Console.SetCursorPosition(1, begin);
                     Console.WriteLine(json);
                     break;
                 case 51: //key 3
@@ -110,10 +115,27 @@ namespace StoneGatewayPoc
                     var httpResponse = serviceClient.Sale.Create(createSaleRequest);
                     var jsonResponse = JsonConvert.SerializeObject(httpResponse, serializerSettings);
                     //
-                    Console.SetCursorPosition(1, 6);
+                    Console.SetCursorPosition(1, begin);
                     Console.Write(new string(' ', Console.WindowWidth));
-                    Console.SetCursorPosition(1, 6);
+                    Console.SetCursorPosition(1, begin);
                     Console.WriteLine(jsonResponse);
+                    break;
+                case 52: //key 4
+                    var orderKey = Guid.Parse("094c775d-ea43-429f-97b9-4474c3fdqwe");
+                    var saleManageResponse = client.Sale.Manage(ManageOperationEnum.Cancel, orderKey);
+
+                    if (saleManageResponse.HttpStatusCode == HttpStatusCode.OK
+                            && saleManageResponse.Response.CreditCardTransactionResultCollection.Any()
+                            && saleManageResponse.Response.CreditCardTransactionResultCollection.All(p => p.Success == true))
+                    {
+                        var respManage = client.Sale.Manage(ManageOperationEnum.Cancel, orderKey).RawResponse;
+                        var objRespManage = JsonConvert.DeserializeObject(respManage);
+                        var jsonRespManage = JsonConvert.SerializeObject(objRespManage, serializerSettings);
+                        Console.SetCursorPosition(1, begin);
+                        Console.Write(new string(' ', Console.WindowWidth));
+                        Console.SetCursorPosition(1, begin);
+                        Console.WriteLine(jsonRespManage);
+                    }
                     break;
                 default:
                     Console.WriteLine("Not found key!");
